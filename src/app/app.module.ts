03db30bb4f901cd72cc11fd6a5086041f945b0a6
algarni <m.t.algarni@gmail.com>
2017-04-28 11:37:03 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,30 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { AboutComponent } from './about/about/about.component';
+import { NotificationComponent } from './notification/notification/notification.component';
+import { NewsComponent } from './news/news/news.component';
+import { MapComponent } from './map/map/map.component';
+import { VideoComponent } from './video/video/video.component';
+import { ImageComponent } from './image/image/image.component';
+import { JobComponent } from './job/job/job.component';
+import { ElearningComponent } from './elearning/elearning/elearning.component';
+import { ContactUsComponent } from './contact-us/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AboutComponent,
+    NotificationComponent,
+    NewsComponent,
+    MapComponent,
+    VideoComponent,
+    ImageComponent,
+    JobComponent,
+    ElearningComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
