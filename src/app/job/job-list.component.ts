b06d6f28dd70dbd50ca3917e2d831922d8c43cc0
algarni <m.t.algarni@gmail.com>
2017-05-16 23:51:44 +0300

    create job-list component

---
+++ src/app/job/job-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-job-list',
+    templateUrl: 'job-list.component.html'
+})
+
+export class JobListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
